17314595f0d69a54ad84dd52a0a7921dfdd36bdc
Lucas Oliveira <lucasnoliveira126@gmail.com>
2024-09-04 12:49:01 -0300

    experiencesummary section responsivity

--- src/app/components/ExperienceSummary.tsx
+++ src/app/components/ExperienceSummary.tsx
@@ -11,7 +11,6 @@ const ExperienceSummary: FC = () => {
         threshold: 0.8,
     });
 
-    // Ref to track whether the animation has occurred
     const [hasAnimated, setHasAnimated] = useState(false);
     const hasAnimatedRef = useRef(false);
     const { translations } = useLanguage();
@@ -27,18 +26,18 @@ const ExperienceSummary: FC = () => {
         <section className="bg-gray-900 py-10" id="experience-summary">
             <div className="container mx-auto px-4">
                 <div className="text-center mb-12">
-                    <h2 className="text-3xl font-bold text-gray-50">{translations.experience}</h2>
+                    <h2 className="text-3xl md:text-4xl font-bold text-gray-50">{translations.experience}</h2>
                 </div>
-                <div ref={ref} className="grid grid-cols-1 md:grid-cols-3 gap-8">
+                <div ref={ref} className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
                     <motion.div
                         initial={{ opacity: 0, y: 50 }}
                         animate={hasAnimated ? { opacity: 1, y: 0 } : {}}
                         transition={{ duration: 0.8, ease: 'easeOut' }}
                     >
                         <ExperienceBlock
-                            icon={<FaLaptopCode className="text-white h-8 w-8" />}
+                            icon={<FaLaptopCode className="text-white h-8 w-8 md:h-10 md:w-10" />}
                             title={translations.ExperienceSummary.techXp.name}
-                            description={hasAnimated ? <CountUp end={3} duration={4} suffix={ ` ${translations.years}`} /> : `${translations.ExperienceSummary.techXp.experienceYears}`} // Duration increased to 4 seconds
+                            description={hasAnimated ? <CountUp end={3} duration={4} suffix={` ${translations.years}`} /> : `${translations.ExperienceSummary.techXp.experienceYears}`}
                             backgroundColor="bg-blue-500"
                             textColor="text-white"
                         />
@@ -49,9 +48,9 @@ const ExperienceSummary: FC = () => {
                         transition={{ duration: 0.8, ease: 'easeOut' }}
                     >
                         <ExperienceBlock
-                            icon={<FaCog className="text-white h-8 w-8" />}
+                            icon={<FaCog className="text-white h-8 w-8 md:h-10 md:w-10" />}
                             title={translations.ExperienceSummary.webDevXp.name}
-                            description={hasAnimated ? <CountUp end={2} duration={4} suffix={ ` ${translations.years}`} /> : `${translations.ExperienceSummary.webDevXp.experienceYears}`} // Duration increased to 4 seconds
+                            description={hasAnimated ? <CountUp end={2} duration={4} suffix={` ${translations.years}`} /> : `${translations.ExperienceSummary.webDevXp.experienceYears}`}
                             backgroundColor="bg-teal-500"
                             textColor="text-white"
                         />
@@ -62,9 +61,9 @@ const ExperienceSummary: FC = () => {
                         transition={{ duration: 0.8, ease: 'easeOut' }}
                     >
                         <ExperienceBlock
-                            icon={<FaProjectDiagram className="text-white h-8 w-8" />}
+                            icon={<FaProjectDiagram className="text-white h-8 w-8 md:h-10 md:w-10" />}
                             title={translations.ExperienceSummary.services.name}
-                            description={hasAnimated ? <CountUp end={27} duration={4} /> : `${translations.ExperienceSummary.services.quantity}`} // Duration increased to 4 seconds
+                            description={hasAnimated ? <CountUp end={27} duration={4} /> : `${translations.ExperienceSummary.services.quantity}`}
                             backgroundColor="bg-purple-500"
                             textColor="text-white"
                         />
@@ -86,13 +85,13 @@ interface ExperienceBlockProps {
 const ExperienceBlock: FC<ExperienceBlockProps> = ({ icon, title, description, backgroundColor, textColor }) => {
     return (
         <div
-            className={`p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ${backgroundColor} ${textColor}`}
+            className={`p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ${backgroundColor} ${textColor} flex flex-col items-center`}
         >
             <div className="flex items-center justify-center mb-4">
                 {icon}
             </div>
-            <h3 className="text-2xl font-semibold mb-2 text-center">{title}</h3>
-            <p className={`text-4xl font-bold text-center ${textColor}`}>{description}</p>
+            <h3 className="text-xl md:text-2xl font-semibold mb-2 text-center">{title}</h3>
+            <p className={`text-3xl md:text-4xl font-bold text-center ${textColor}`}>{description}</p>
         </div>
     );
 };
