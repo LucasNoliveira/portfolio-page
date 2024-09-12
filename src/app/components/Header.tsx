42ce9512d99925b749d7c47dc3562eface0eb562
Lucas Oliveira <lucasnoliveira126@gmail.com>
2024-09-12 17:59:13 -0300

    download cv

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useRef, useEffect } from 'react';
 import Link from 'next/link';
-import { Bars3Icon, XMarkIcon, ChevronDownIcon, GlobeAltIcon } from '@heroicons/react/24/outline';
+import { ChevronDownIcon, GlobeAltIcon } from '@heroicons/react/24/outline';
 import { useLanguage } from '../context/LanguageContext';
 
 const Header: React.FC = () => {
@@ -48,6 +48,12 @@ const Header: React.FC = () => {
         setIsLanguageOpen(false);
     };
 
+    const getCVLink = () => {
+        return language === 'PT'
+            ? '/cv/cv-update.pdf'
+            : '/cv/Lucas Neves Oliveira - english cv.pdf';
+    };
+
     return (
         <header className="bg-gray-100 fixed w-full z-10 top-0 left-0">
             <div className="container mx-auto flex justify-between items-center p-5 relative">
@@ -72,7 +78,7 @@ const Header: React.FC = () => {
                         <div
                             className={`absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-md overflow-hidden transition-all duration-300 ease-in-out transform ${isDropdownOpen ? 'max-h-40 opacity-100' : 'max-h-0 opacity-0'}`}
                         >
-                            <Link href="/curriculo.pdf" download onClick={() => setIsDropdownOpen(false)} className="block px-4 py-2 text-black-600 hover:bg-gray-100">{translations.downloadCV}</Link>
+                            <Link href={getCVLink()} download onClick={() => setIsDropdownOpen(false)} className="block px-4 py-2 text-black-600 hover:bg-gray-100">{translations.downloadCV}</Link>
                             <Link href="/curriculo-online" onClick={() => setIsDropdownOpen(false)} className="block px-4 py-2 text-black-600 hover:bg-gray-100">{translations.readOnline}</Link>
                         </div>
                     </div>
@@ -134,31 +140,22 @@ const Header: React.FC = () => {
 
             <div className={`fixed inset-0 bg-white bg-opacity-80 backdrop-blur-sm overflow-hidden transition-transform ${isOpen ? 'translate-x-0 duration-200 ease-in' : 'translate-x-full duration-500 ease-out'}`}>
                 <nav className={`flex flex-col h-full items-center justify-center space-y-4 p-6 transition-opacity ${isOpen ? 'opacity-100 duration-300 ease-in' : 'opacity-0 duration-500 ease-out'}`}>
-                    <Link href="/" onClick={toggleMenu} className="text-gray-700 text-lg font-semibold hover:text-blue-600 transition duration-300">{translations.home}</Link>
-                    <Link href="/about" onClick={toggleMenu} className="text-gray-700 text-lg font-semibold hover:text-blue-600 transition duration-300">{translations.about}</Link>
-                    <Link href="/experience" onClick={toggleMenu} className="text-gray-700 text-lg font-semibold hover:text-blue-600 transition duration-300">{translations.experience}</Link>
-                    <Link href="#services" onClick={toggleMenu} className="text-gray-700 text-lg font-semibold hover:text-blue-600 transition duration-300">{translations.services}</Link>
-                    <Link href="/projects" onClick={toggleMenu} className="text-gray-700 text-lg font-semibold hover:text-blue-600 transition duration-300">{translations.projects}</Link>
-                    <Link href="/contact" onClick={toggleMenu} className="text-gray-700 text-lg font-semibold hover:text-blue-600 transition duration-300">{translations.contact}</Link>
-                    <div className="w-full flex justify-center space-x-2 mt-4">
-                        <Link href="/curriculo.pdf" download onClick={toggleMenu} className="bg-blue-600 text-white px-5 py-3 rounded-lg shadow-md flex items-center hover:bg-blue-700 transition duration-300">
+                    <Link href="/" onClick={toggleMenu} className="font-bold text-xl text-[#333] hover:text-blue-600 transition duration-300">{translations.home}</Link>
+                    <Link href="/about" onClick={toggleMenu} className="font-bold text-xl text-[#333] hover:text-blue-600 transition duration-300">{translations.about}</Link>
+                    <Link href="#experiences" onClick={toggleMenu} className="font-bold text-xl text-[#333] hover:text-blue-600 transition duration-300">{translations.experience}</Link>
+                    <Link href="#services" onClick={toggleMenu} className="font-bold text-xl text-[#333] hover:text-blue-600 transition duration-300">{translations.services}</Link>
+                    <Link href="/projects" onClick={toggleMenu} className="font-bold text-xl text-[#333] hover:text-blue-600 transition duration-300">{translations.projects}</Link>
+                    <Link href="/contact" onClick={toggleMenu} className="font-bold text-xl text-[#333] hover:text-blue-600 transition duration-300">{translations.contact}</Link>
+                    <div className="relative" ref={dropdownRef}>
+                        <button onClick={toggleDropdown} className="bg-blue-600 text-white px-4 py-2 rounded flex items-center hover:bg-blue-700 transition duration-300">
                             {translations.downloadCV}
-                        </Link>
-                        <Link href="/curriculo-online" onClick={toggleMenu} className="bg-blue-600 text-white px-5 py-3 rounded-lg shadow-md flex items-center hover:bg-blue-700 transition duration-300">
-                            {translations.readOnline}
-                        </Link>
-                    </div>
-                    <div className="relative mt-4 hidden">
-                        <button onClick={toggleLanguageDropdown} className="flex items-center space-x-2 text-gray-800 hover:text-blue-600 transition duration-300">
-                            <GlobeAltIcon className="h-6 w-6" />
-                            <span>{language}</span>
-                            <ChevronDownIcon className="h-5 w-5" />
+                            <ChevronDownIcon className="h-5 w-5 ml-2" />
                         </button>
-                        <div className={`absolute right-0 mt-2 w-32 bg-white shadow-lg rounded-md overflow-hidden transition-opacity duration-300 ease-in-out ${isLanguageOpen ? 'opacity-100' : 'opacity-0'}`}>
-                            <button className="block px-4 py-2 text-black hover:bg-gray-100 w-full text-left" onClick={() => handleLanguageChange('EN')}>English</button>
-                            <button className="block px-4 py-2 text-black hover:bg-gray-100 w-full text-left" onClick={() => handleLanguageChange('PT')}>Português</button>
-                            <button className="block px-4 py-2 text-black hover:bg-gray-100 w-full text-left" onClick={() => handleLanguageChange('ES')}>Español</button>
-                            <button className="block px-4 py-2 text-black hover:bg-gray-100 w-full text-left" onClick={() => handleLanguageChange('JP')}>日本語</button>
+                        <div
+                            className={`absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-md overflow-hidden transition-all duration-300 ease-in-out transform ${isDropdownOpen ? 'max-h-40 opacity-100' : 'max-h-0 opacity-0'}`}
+                        >
+                            <Link href={getCVLink()} download onClick={() => setIsDropdownOpen(false)} className="block px-4 py-2 text-black-600 hover:bg-gray-100">{translations.downloadCV}</Link>
+                            <Link href="/curriculo-online" onClick={() => setIsDropdownOpen(false)} className="block px-4 py-2 text-black-600 hover:bg-gray-100">{translations.readOnline}</Link>
                         </div>
                     </div>
                 </nav>
