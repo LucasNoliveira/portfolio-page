d9d7cadfcdec1d51e2e76b0f5719603633afb321
Lucas Oliveira <lucasnoliveira126@gmail.com>
2024-09-24 10:10:51 -0300

    custom scrollbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        scrollbar: '#b8c1ec',
+        scrollbarHover: '#8590c3',
+        scrollbarThumb: '#515ea3',
+      },
     },
   },
   plugins: [],
