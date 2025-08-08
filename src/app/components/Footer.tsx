cf437ed554363ee8bce698e93330c2f1d69bff71
Lucas Oliveira <lucasnoliveira126@gmail.com>
2025-08-08 12:44:45 -0300

    responsive base text font

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -11,12 +11,11 @@ const Footer: FC = () => {
         <footer className="bg-gray-900 text-gray-200 py-12 md:py-16 lg:py-20">
             <div className="container mx-auto px-6 md:px-8 lg:px-12">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                    {/* Brand & Description */}
                     <div className="flex flex-col items-center md:items-start">
                         <Link href="/" className="text-3xl font-bold text-white mb-4">
                             Lucas Oliveira
                         </Link>
-                        <p className="text-center md:text-left text-gray-400 text-sm md:text-base">
+                        <p className="text-center md:text-left text-gray-400 text-lg md:text-base">
                             {translations.elevatingYourWebXp}
                         </p>
                     </div>
@@ -24,21 +23,21 @@ const Footer: FC = () => {
                     <div className="flex flex-col items-center md:items-start">
                         <h3 className="text-xl font-semibold mb-4">{translations.quickLinks}</h3>
                         <nav className="flex flex-col space-y-2">
-                            <Link href="#about" className="hover:text-white transition-colors duration-300 text-sm md:text-base">
+                            <Link href="#about" className="hover:text-white transition-colors duration-300 text-lg md:text-base">
                                 {translations.about}
                             </Link>
-                            <Link href="#projects" className="hover:text-white transition-colors duration-300 text-sm md:text-base">
+                            <Link href="#projects" className="hover:text-white transition-colors duration-300 text-lg md:text-base">
                                 {translations.projects}
                             </Link>
-                            <Link href="#contact" className="hover:text-white transition-colors duration-300 text-sm md:text-base">
+                            <Link href="#contact" className="hover:text-white transition-colors duration-300 text-lg md:text-base">
                                 {translations.contact}
                             </Link>
                         </nav>
                     </div>
 
                     <div className="flex flex-col items-center md:items-start">
-                        <h3 className="text-xl font-semibold mb-4">{translations.contactUs}</h3>
-                        <ul className="space-y-2 text-sm md:text-base">
+                        <h3 className="text-lg font-semibold mb-4">{translations.contactUs}</h3>
+                        <ul className="space-y-2 text-lg md:text-base">
                             <li className="flex items-center">
                                 <FaEnvelope className="text-teal-500 mr-2" />
                                 <a href="mailto:lucasnoliveira126@gmail.com" className="hover:text-white hover:underline transition-colors duration-300">
